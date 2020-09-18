9945048c7e979094031bf1f29ac8b7b82dea094a
menahishayan <menahi.shayan@gmail.com>
2020-09-18 23:53:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TP-Link Archer C50 API
+ HTTP based python script for API access and router management of the TP-Link Archer C50
---
+++ test.py
@@ -0,0 +1,47 @@
+from aiohttp.hdrs import (
+    COOKIE,
+    REFERER,
+    CONTENT_TYPE
+)
+import base64
+import requests
+import json
+
+hostname = '192.168.0.1'
+username = 'admin'
+password = 'alphatango'
+
+def get_base64_cookie_string():
+    username_password = '{}:{}'.format(username, password)
+    b64_encoded_username_password = base64.b64encode(
+        username_password.encode('ascii')
+    ).decode('ascii')
+    return 'Authorization=Basic {}'.format(b64_encoded_username_password)
+
+def req_parse(body):
+    bodyStr = ''
+    for b in body:
+        bodyStr += b + '\r\n'
+    return bodyStr
+
+def res_parse(res):
+    items = {}
+    for b in res.splitlines():
+        if '=' in b:
+            items[b[0:b.index('=')]] = b[b.index('=')+1:]
+    return items
+
+cookie = get_base64_cookie_string()
+
+referer = 'http://{}'.format(hostname)
+
+with open('/Users/shayan/Desktop/Projects/router/req.json') as f:
+  ref = json.load(f)
+
+page = requests.post(
+    'http://{}/cgi?{}'.format(hostname,ref['version']['path']),
+    headers={REFERER: referer, COOKIE: cookie},
+    data=(req_parse(ref['version']['body'])),
+    timeout=4)
+    
+print(res_parse(page.text))
